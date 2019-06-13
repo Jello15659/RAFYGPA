0b00aaa108948eeb6302f793a37c2d0f956c9a01
dzhong8 <50762060+dzhong8@users.noreply.github.com>
2019-06-13 02:07:16 -0400

    lolz

---
+++ RAFYGPA-master/what_am_i_doing/Assets/Scripts/IgnoreCollisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisions : MonoBehaviour
+{
+    public GameObject second;
+
+    void Update()
+    {
+        Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), second.GetComponent<Collider2D>());
+    }
+    
+}
